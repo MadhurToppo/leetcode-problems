7d70a0a26b02491950d87948f92c94e84b7055da
madhurtoppo <madhur.toppo@gmail.com>
2021-05-02 16:48:08 +0530

    Added Search in Rotated Sorted Array with tests

---
+++ src/main/java/com/madhurtoppo/leetcode/arrays/SearchInRotatedSorted.java
@@ -0,0 +1,44 @@
+package com.madhurtoppo.leetcode.arrays;
+
+public class SearchInRotatedSorted {
+
+    public int method2n1(int[] nums, int target) {
+        var left = 0;
+        var right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[right]) {
+                right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        int minPos = left;
+        left = 0;
+        right = nums.length - 1;
+
+        if (target >= nums[minPos] && target <= nums[right]) {
+            left = minPos;
+        } else {
+            right = minPos;
+        }
+
+        while (left <= right) {
+            int mid = left + (right - left) / 2;
+
+            if (target == nums[mid]) {
+                return mid;
+            } else if (target < nums[mid]) {
+                right = mid - 1;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return -1;
+    }
+}
+
