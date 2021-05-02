a125d742a3bd1866e136026e95dc9a02e85b1e46
madhurtoppo <madhur.toppo@gmail.com>
2021-05-02 16:19:47 +0530

    Added Find Minimum in Rotated Sorted Array Problem with tests

---
+++ src/main/java/com/madhurtoppo/leetcode/arrays/MinInRotatedSorted.java
@@ -0,0 +1,21 @@
+package com.madhurtoppo.leetcode.arrays;
+
+public class MinInRotatedSorted {
+
+    public int method2(int[] nums) {
+        var left = 0;
+        var right = nums.length - 1;
+
+        while (left < right) {
+            int mid = left + (right - left) / 2;
+
+            if (nums[mid] < nums[right]) {
+               right = mid;
+            } else {
+                left = mid + 1;
+            }
+        }
+
+        return nums[left];
+    }
+}
