58067b17e995932b02f1cc6a0e6024fc0d544a6d
madhurtoppo <madhur.toppo@gmail.com>
2021-05-06 22:16:30 +0530

    Added In Order Traversal

---
+++ src/main/java/com/madhurtoppo/leetcode/tree/InOrderTraversal.java
@@ -0,0 +1,56 @@
+package com.madhurtoppo.leetcode.tree;
+
+import com.madhurtoppo.leetcode.commons.classes.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Stack;
+
+public class InOrderTraversal {
+
+    public static class Recursive {
+
+        public List<Integer> inOrder(TreeNode root) {
+            ArrayList<Integer> values = new ArrayList<>();
+            return helper(root, values);
+        }
+
+        private List<Integer> helper(TreeNode root, List<Integer> values) {
+            if (root == null) {
+                return values;
+            }
+
+            helper(root.left, values);
+            values.add(root.val);
+            helper(root.right, values);
+
+            return values;
+        }
+    }
+
+    public static class Iterative {
+        public List<Integer> inOrder(TreeNode root) {
+            ArrayList<Integer> values = new ArrayList<>();
+
+            if (root == null) {
+                return values;
+            }
+
+            Stack<TreeNode> nodes = new Stack<>();
+
+            while (root != null || !nodes.empty()) {
+
+                while (root != null) {
+                    nodes.push(root);
+                    root = root.left;
+                }
+
+                root = nodes.pop();
+                values.add(root.val);
+                root = root.right;
+            }
+
+            return values;
+        }
+    }
+}
