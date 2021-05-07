50fb9913520353d162d0d00f012dd4edf07cd203
madhurtoppo <madhur.toppo@gmail.com>
2021-05-08 00:27:31 +0530

    Added Same Tree Problem

---
+++ src/main/java/com/madhurtoppo/leetcode/tree/SameTree.java
@@ -0,0 +1,20 @@
+package com.madhurtoppo.leetcode.tree;
+
+import com.madhurtoppo.leetcode.commons.classes.TreeNode;
+
+public class SameTree {
+    public static class Recursive {
+        public boolean isSameTree(TreeNode p, TreeNode q) {
+            if (p == null && q == null) {
+                return true;
+            }
+            if (p == null || q == null) {
+                return false;
+            }
+            if (p.val != q.val) {
+                return false;
+            }
+            return isSameTree(p.left, q.left) && isSameTree(p.right, q.right);
+        }
+    }
+}
---
+++ src/test/java/com/madhurtoppo/leetcode/tree/SameTreeTest.java
@@ -0,0 +1,55 @@
+package com.madhurtoppo.leetcode.tree;
+
+import com.madhurtoppo.leetcode.commons.classes.TreeNode;
+import com.madhurtoppo.leetcode.commons.utils.TreeUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+class SameTreeTest {
+
+    private static SameTree.Recursive recursive;
+
+    private static TreeNode p;
+    private static TreeNode q;
+
+    private static boolean condition;
+
+    @BeforeEach
+    void setUp() {
+        recursive = new SameTree.Recursive();
+    }
+
+    @Test
+    void recursiveTest1() {
+        p = TreeUtils.constructBinaryTree(Arrays.asList(1,2,3,4,null,null,5,6));
+        q = TreeUtils.constructBinaryTree(Arrays.asList(1,2,3,4,null,null,5,6));
+
+        condition = recursive.isSameTree(p, q);
+
+        assertTrue(condition);
+    }
+
+    @Test
+    void recursiveTest2() {
+        p = TreeUtils.constructBinaryTree(Arrays.asList(1,2,3,4,null,null,5,6));
+        q = TreeUtils.constructBinaryTree(Arrays.asList(1,2,3,4,null,null,5,7));
+
+        condition = recursive.isSameTree(p, q);
+
+        assertFalse(condition);
+    }
+
+    @Test
+    void recursiveTest3() {
+        p = TreeUtils.constructBinaryTree(Arrays.asList(1,2,3,4,null,null,5,6));
+        q = TreeUtils.constructBinaryTree(Arrays.asList(1,2,3,4,null,null,7));
+
+        condition = recursive.isSameTree(p, q);
+
+        assertFalse(condition);
+    }
+}
