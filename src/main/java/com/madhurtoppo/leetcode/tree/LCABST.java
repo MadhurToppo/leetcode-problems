3d727802c89af4e209799921fac383472eee7d2e
madhurtoppo <madhur.toppo@gmail.com>
2021-05-08 18:16:03 +0530

    Added Lowest Common Ancestor in Binary Search Tree Problem

---
+++ src/main/java/com/madhurtoppo/leetcode/tree/LCABST.java
@@ -0,0 +1,21 @@
+package com.madhurtoppo.leetcode.tree;
+
+import com.madhurtoppo.leetcode.commons.classes.TreeNode;
+
+public class LCABST {
+
+    public static class Recursive {
+        public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+            if (root == null) {
+                return null;
+            }
+            if (p.val < root.val && q.val < root.val) {
+                return lowestCommonAncestor(root.left, p, q);
+            } else if (p.val > root.val && q.val > root.val) {
+                return lowestCommonAncestor(root.right, p, q);
+            } else {
+                return root;
+            }
+        }
+    }
+}
---
+++ src/test/java/com/madhurtoppo/leetcode/tree/LCABSTTest.java
@@ -0,0 +1,53 @@
+package com.madhurtoppo.leetcode.tree;
+
+import com.madhurtoppo.leetcode.commons.classes.TreeNode;
+import com.madhurtoppo.leetcode.commons.utils.TreeUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Arrays;
+
+class LCABSTTest {
+
+    private static LCABST.Recursive recursive;
+
+    private static TreeNode root;
+    private static TreeNode p;
+    private static TreeNode q;
+
+    private static TreeNode actual;
+    private static TreeNode expected;
+
+    @BeforeEach
+    void setUp() {
+        recursive = new LCABST.Recursive();
+    }
+
+    @Test
+    void lowestCommonAncestorTest1() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(6,2,8,0,4,7,9,null,null,3,5));
+
+        p = TreeUtils.constructBinaryTree(Arrays.asList(2,0,4,null,null,3,5));
+        q = TreeUtils.constructBinaryTree(Arrays.asList(8,7,9));
+
+        actual = recursive.lowestCommonAncestor(root, p , q);
+        expected = TreeUtils.constructBinaryTree(Arrays.asList(6,2,8,0,4,7,9,null,null,3,5));
+
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void lowestCommonAncestorTest2() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(6,2,8,0,4,7,9,null,null,3,5));
+
+        p = TreeUtils.constructBinaryTree(Arrays.asList(2,0,4,null,null,3,5));
+        q = TreeUtils.constructBinaryTree(Arrays.asList(4,3,5));
+
+        actual = recursive.lowestCommonAncestor(root, p , q);
+        expected = TreeUtils.constructBinaryTree(Arrays.asList(2,0,4,null,null,3,5));
+
+        assertEquals(expected, actual);
+    }
+}
