c1a4fb0b1e97b792493d123dba4268f6290e0c2b
madhurtoppo <madhur.toppo@gmail.com>
2021-05-14 18:13:56 +0530

    Added test for Rotate Matrix

--- src/test/java/com/madhurtoppo/leetcode/arrays/RotateMatrixTest.java
+++ src/test/java/com/madhurtoppo/leetcode/arrays/RotateMatrixTest.java
@@ -21,7 +21,7 @@ class RotateMatrixTest {
     }
 
     @Test
-    void rotate() {
+    void rotateTest1() {
         matrix = new int[][]{
                 {1,2,3},
                 {4,5,6},
@@ -36,7 +36,27 @@ class RotateMatrixTest {
                 {8,5,2},
                 {9,6,3}
         };
+        assertArrayEquals(expected, actual);
+    }
 
+    @Test
+    void rotateTest2() {
+        matrix = new int[][]{
+                {1,2,3,4},
+                {5,6,7,8},
+                {9,10,11,12},
+                {13,14,15,16}
+        };
+
+        rotateMatrix.rotate(matrix);
+        actual = matrix;
+
+        expected = new int[][]{
+                {13,9,5,1},
+                {14,10,6,2},
+                {15,11,7,3},
+                {16,12,8,4}
+        };
         assertArrayEquals(expected, actual);
     }
 }
