a68cc5892e6627ca1a9faa29c2751ff13c2fca1b
madhurtoppo <madhur.toppo@gmail.com>
2021-05-16 23:41:17 +0530

    Added Combination Sum

---
+++ src/main/java/com/madhurtoppo/leetcode/graph/CombinationSum.java
@@ -0,0 +1,29 @@
+package com.madhurtoppo.leetcode.graph;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CombinationSum {
+    public List<List<Integer>> combination(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+        dfs( candidates, new ArrayList<Integer>(), result,0, target);
+        return result;
+    }
+
+    private void dfs(int[] candidates, List<Integer> list, List<List<Integer>> result, int index, int target) {
+        if (target == 0) {
+            result.add(new ArrayList<Integer>(list));
+            return;
+        }
+        if (target < 0) {
+            return;
+        }
+
+        for (int i = index; i < candidates.length; i++) {
+            list.add(candidates[i]);
+            dfs(candidates, list, result, i, target - candidates[i]);
+            list.remove(list.size() - 1);
+        }
+    }
+
+}
---
+++ src/test/java/com/madhurtoppo/leetcode/graph/CombinationSumTest.java
@@ -0,0 +1,47 @@
+package com.madhurtoppo.leetcode.graph;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class CombinationSumTest {
+
+    private CombinationSum combinationSum;
+    private int[] candidates;
+    private int target;
+    private List<List<Integer>> actual;
+    private List<List<Integer>> expected;
+
+    @BeforeEach
+    void setUp() {
+        combinationSum = new CombinationSum();
+    }
+
+    @Test
+    void combinationTest1() {
+        candidates = new int[]{2,3,6,7};
+        target = 7;
+        actual = combinationSum.combination(candidates, target);
+        expected = new ArrayList<>();
+        expected.add(Arrays.asList(2, 2, 3));
+        expected.add(Arrays.asList(7));
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void combinationTest2() {
+        candidates = new int[]{2,3,5};
+        target = 8;
+        actual = combinationSum.combination(candidates, target);
+        expected = new ArrayList<>();
+        expected.add(Arrays.asList(2, 2, 2, 2));
+        expected.add(Arrays.asList(2, 3, 3));
+        expected.add(Arrays.asList(3, 5));
+        assertEquals(expected, actual);
+    }
+}
