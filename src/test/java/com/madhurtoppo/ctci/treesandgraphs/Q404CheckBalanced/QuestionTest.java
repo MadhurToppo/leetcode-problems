05a31df793495aaa9a2922dc6e4b3b3ceeed062d
madhurtoppo <madhur.toppo@gmail.com>
2021-07-22 18:14:02 +0530

    Added Check Balanced Tree Question

---
+++ src/test/java/com/madhurtoppo/ctci/treesandgraphs/Q404CheckBalanced/QuestionTest.java
@@ -0,0 +1,37 @@
+package com.madhurtoppo.ctci.treesandgraphs.Q404CheckBalanced;
+
+import com.madhurtoppo.leetcode.commons.classes.TreeNode;
+import com.madhurtoppo.leetcode.commons.utils.TreeUtils;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.Arrays;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionTest {
+
+    private static Question.Recursive recursive;
+    private static TreeNode root;
+
+    private static boolean condition;
+
+    @BeforeEach
+    void setUp() {
+        recursive = new Question.Recursive();
+    }
+
+    @Test
+    void isBalancedTest1() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(3,9,20,null,null,15,7));
+        condition = recursive.isBalanced(root);
+        assertTrue(condition);
+    }
+
+    @Test
+    void isBalancedTest2() {
+        root = TreeUtils.constructBinaryTree(Arrays.asList(1,2,2,3,3,null,null,4,4));
+        condition = recursive.isBalanced(root);
+        assertFalse(condition);
+    }
+}
