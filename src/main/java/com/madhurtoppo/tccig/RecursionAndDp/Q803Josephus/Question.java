5ba64690e08c90715985e86744e402de2379f5e8
madhurtoppo <madhur.toppo@gmail.com>
2021-08-19 17:03:06 +0530

    Added comments

--- src/main/java/com/madhurtoppo/tccig/RecursionAndDp/Q803Josephus/Question.java
+++ src/main/java/com/madhurtoppo/tccig/RecursionAndDp/Q803Josephus/Question.java
@@ -5,7 +5,10 @@ public class Question {
         if (n == 1) {
             return 1;
         } else {
-            // Subtract 1 from inside and add 1 outside as it is one-indexed
+
+            // (k + 1 + josephus(n - 1, k) - 1) {Move to k + 1 position and subtract 1 after killing 1 person}
+            // (k + 1 + josephus(n - 1, k) - 1) % n {To wrap into circle}
+            // (k + 1 + josephus(n - 1, k) - 2) % n + 1 {-1 from inside and +1 from outside as it is one-indexed}
             return (k + 1 + josephus(n - 1, k) - 2) % n + 1;
         }
     }
