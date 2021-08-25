5b661d4ae888868825cc30aaa5693a8f09b8d7b5
madhurtoppo <madhur.toppo@gmail.com>
2021-08-25 23:20:44 +0530

    Added String Permutations Problem

---
+++ src/test/java/com/madhurtoppo/tccig/dpandrecursion/q810string_permutations/QuestionTest.java
@@ -0,0 +1,37 @@
+package com.madhurtoppo.tccig.dpandrecursion.q810string_permutations;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class QuestionTest {
+
+    private Question question;
+    private String str;
+    private List<String> expected;
+    private List<String> actual;
+
+    @BeforeEach
+    void setUp() {
+        question = new Question();
+    }
+
+    @Test
+    void test1() {
+        str = "ABC";
+        expected = new ArrayList<>();
+        expected.add("ABC");
+        expected.add("ACB");
+        expected.add("BAC");
+        expected.add("BCA");
+        expected.add("CAB");
+        expected.add("CBA");
+        actual = question.permute(str);
+
+        assertEquals(expected, actual);
+    }
+}
